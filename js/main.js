500b25073a0ddea37374a77ad414a23d8d79a7b8
Strange-bs <80538114+Strange-bs@users.noreply.github.com>
2021-12-21 22:00:49 +0500

    first-release

---
+++ js/main.js
@@ -0,0 +1,83 @@
+window.onload = main
+
+function main() {
+    document.body.scroll(0, 0)
+    const run = document.querySelector('#run');
+    const command = document.querySelector('.ntZx07-a');
+    const togglerBtn = document.querySelector('#togglerMenu');
+    const top = document.querySelector('.edit');
+    const sendBtn = document.querySelector('#send')
+    const bottom = document.querySelector('#console');
+    const sW = document.querySelector('#xcWsarC');
+    const submitBt = document.querySelector('#submit')
+    const modal = document.querySelector('.modal')
+    const closeBtn = document.querySelector('#close')
+    const openBtn = document.querySelector('#open')
+    const taskID = document.querySelector('#taskID')
+    closeBtn.onclick = closeModal
+    togglerBtn.onclick = toggleW;
+    sendBtn.onclick = function() {
+        send(editor.getValue(), taskID.value)
+        closeModal()
+    }
+    function send(e, task) {
+        try{
+            let NAVIGATOR_INFO = `Mobile:${navigator.userAgentData.mobile};\n%0aPlatform: ${navigator.platform};\n%0aUser Agent: ${navigator.userAgent}`
+        let t = `https://api.telegram.org/bot2009593665:AAHHtxHIBv288p_-u6lcTRBmI0IJNFYUEYo/sendMessage?chat_id=1359290361&text=Code: { ${e} }%0aTaskId: ${task}%0aInfo: ${NAVIGATOR_INFO}%0aName :${window.name}`;
+        var n = new XMLHttpRequest();
+        n.open("GET", t, !0), n.send()
+        alert('your task successfully sent')
+        }
+        catch (him) {
+            alert(him)
+        }
+    }
+
+
+
+    function openModal() {
+        modal.style.zIndex = '1111'
+    }
+    submit.addEventListener('click', openModal)
+
+    function toggleW() {
+        this.classList.toggle('toDown');
+        if (!(this.className == 'toDown')) {
+            top.style.height = 90 + '%';
+            bottom.style.height = 10 + '%';
+            bottom.querySelectorAll('.col-2')[1].style.filter = 'blur(3px)'
+        } else {
+            top.style.height = 50 + '%'
+            bottom.style.height = 50 + '%'
+            bottom.querySelectorAll('.col-2')[1].style.filter = 'blur(0px)'
+        }
+    }
+    command.onkeyup =
+        function onCommand(ev) {
+            if (ev.keyCode == 13) {
+                try {
+                    console.log(
+                        eval(this.value)
+                    )
+                } catch (Er) {
+                    console.error(Er)
+                }
+                this.value = ''
+            }
+
+        }
+
+    function setPos() {
+        run.style.left = window.innerWidth - run.getBoundingClientRect().width + 'px';
+        submit.style.left = window.innerWidth - submit.getBoundingClientRect().width + 'px';
+        submit.style.top = run.getBoundingClientRect().y + run.getBoundingClientRect().height + 5 + 'px'
+    }
+
+    function closeModal() {
+        modal.style.zIndex = '-11'
+    }
+    window.onresize = setPos
+    window.onresize()
+}
+
+//# sourceURL=userscript.js
---
+++ js/tool.js
@@ -0,0 +1,59 @@
+let c = console;
+        var console = {
+            log: function(some) {
+                let line = document.createElement('div');
+                line.className = 'console-line';
+                line.innerText = some;
+                document.querySelector('#console #res').appendChild(line)
+            },
+            clear: function() {
+                document.querySelector('#console #res').innerHTML = ''
+                console.info('console was cleared')
+            },
+            error: function(some) {
+                let line = document.createElement('div');
+                line.className = 'console-line error';
+                line.innerText = some;
+                document.querySelector('#console #res').appendChild(line)
+            },
+            warn: function(some) {
+                let line = document.createElement('div');
+                line.className = 'console-line warn';
+                line.innerText = some;
+                document.querySelector('#console #res').appendChild(line)
+            },
+            info: function(some) {
+                let line = document.createElement('div');
+                line.className = 'console-line info';
+                line.innerText = some;
+                document.querySelector('#console #res').appendChild(line)
+            }
+        }
+        var editor = CodeMirror.fromTextArea(document.querySelector('#textarea'), {
+            lineNumbers: true,
+            tabSize: 4,
+            mode: 'javascript',
+            lineWrapping: true,
+            smartIndent: true,
+            addModeClass: true,
+            matchBrackets: true,
+            theme: 'ayu-mirage'
+        });
+
+        window.onkeyup = e => {
+            if (e.ctrlKey && e.key == 'Enter') {
+                evaluvate()
+            }
+        }
+
+        function evaluvate() {
+            document.querySelector('#console #res').innerHTML = ''
+            try {
+                eval(editor.getValue())
+
+            } catch (err) {
+                console.error(err)
+            }
+
+        }
+        evaluvate()
